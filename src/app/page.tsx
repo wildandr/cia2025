8d1aa1e26e88d1b22d3d594219d231128f596ef8
Whyaziz <rizkyaziz214@gmail.com>
2025-03-21 22:20:39 +0700

    feat: logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
 import Image from "next/image";
+import Hero from "@/components/home/Hero";
 
 export default function Home() {
   return (
-    <>
-      <div className="flex min-h-screen flex-col justify-center items-center bg-white text-black">
-        <p className=" font-bold text-xl">Test Teks</p>
-        <p className=" font-bold text-xl font-openSans">Test Teks</p>
-        <p className=" font-bold text-xl font-plusJakarta">Test Teks</p>
-      </div>
-      <div className="flex min-h-screen flex-col justify-center items-center bg-white text-black">
-        <p className=" font-bold text-xl">Test Teks</p>
-        <p className=" font-bold text-xl font-openSans">Test Teks</p>
-        <p className=" font-bold text-xl font-plusJakarta">Test Teks</p>
-      </div>
-    </>
+    <div className="">
+      <Hero />
+    </div>
   );
 }
